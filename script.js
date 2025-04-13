acd08fa8f108b96372fc48259877deb932ae3a2d
Mistborn112 <aasiyah.elshazly@edu.sait.ca>
2025-04-13 12:17:01 -0600

    update js

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-
-const apiKey = "fdcb705195fcb7c8bed32a59408837ae"; // Replace with your OpenWeatherMap API key
-const city = "Cancun,mx";
-const url = `https://api.openweathermap.org/data/2.5/weather?q=cancun,mx&units=metric&appid=fdcb705195fcb7c8bed32a59408837ae`;
-
-fetch(url)
-.then(response => response.json())
-.then(data => {
-    const temp = Math.round(data.main.temp);
-    const wind = Math.round(data.wind.speed);
-    const weatherBox = document.getElementById("weather");
-    weatherBox.textContent = `${temp}°C | Wind: ${wind} kph`;
-})
-.catch(() => {
-      document.getElementById("weather").textContent = "Weather unavailable";
-});
+fetch(
+	'https://api.openweathermap.org/data/2.5/weather?q=cancun,mx&units=metric&appid=fdcb705195fcb7c8bed32a59408837ae'
+)
+	.then(response => response.json())
+	.then(data => {
+		console.log(data);
+		console.log(data.main.temp);
+		temperature.textContent =
+			'Temperature: ' + data.main.temp + '\u00B0C';
+		description.textContent =
+			'Description: ' + data.weather[0].description;
+        });
 
